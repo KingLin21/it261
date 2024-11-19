c8ab920edde76a54795a8ca3948dcb4b9d3f1b36
KingLin21 <117612761+KingLin21@users.noreply.github.com>
2024-11-18 23:48:56 -0800

    Image table

---
+++ website/thx.php
@@ -0,0 +1,18 @@
+<?php
+include('config.php');
+include('includes/header.php');
+?>
+    <div id="wrapper">
+    
+    
+    <main>
+        <h1>Thank you for your form submission</h1>
+        <p>We have reached out to your email! Remember to check SPAM!</p>
+    </main>
+
+
+    </div>
+    <!--end of wrapper-->
+
+    <?php
+    include('./includes/footer.php');
